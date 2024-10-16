4e68d3faa980bfd0e1c13eeb3d59a3f38c8f495d
Tong Yung <tohow15@gmail.com>
2024-10-16 18:52:34 +0800

    commit

--- lib/tutorial/homepage1.dart
+++ lib/tutorial/homepage1.dart
@@ -15,12 +15,20 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //appbar used to display the top bar
       appBar: AppBar(
         backgroundColor: Colors.orange[300],
         title: const Text("Personal Health Assistant",
             style: TextStyle(fontSize: 20)),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
       ),
+      //This is used to display the bottom bar
       bottomNavigationBar: BottomAppBar(
           color: Colors.blue,
           height: 50,
@@ -28,7 +36,29 @@ class Homepage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SecondPage(),
+                    ),
+                  );
+                },
+              ),
+              IconButton(
                 icon: const Icon(Icons.home),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SecondPage(),
+                    ),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.info),
                 onPressed: () {
                   Navigator.push(
                     context,
@@ -44,32 +74,49 @@ class Homepage extends StatelessWidget {
         color: Colors.grey[200],
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.start, // Aligns children at the top
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  color: Colors.red[400],
-                  height: 100,
-                  alignment: Alignment.center,
+                padding: const EdgeInsets.all(15.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Card(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image.asset(
+                        "images/profile_picture.jpg",
+                        width: 150,
+                        fit: BoxFit.fill,
+                      ),
+                      const SizedBox(height: 8.0),
+                      const Text(
+                        'Dinesh Kumar',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  )),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Container(
                   color: Colors.red[400],
-                  height: 100,
+                  height: 250,
                   alignment: Alignment.center,
                 ),
               ),
-              // Add GridView here
               Column(
                 children: [
                   Container(
                     height: 600, // Set a height for the GridView
                     child: GridView.count(
                       crossAxisCount: 2, // Number of columns
+                      physics:
+                          const NeverScrollableScrollPhysics(), // Disable GridView scrolling
                       children: <Widget>[
                         Padding(
                           padding: const EdgeInsets.all(20.0),
